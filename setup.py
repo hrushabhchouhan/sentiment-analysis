8ffa364ed8d0c5bc29d0c72e3c337045cd3982a5
Hrushabhsingh Chouhan <43321682+hrushabhchouhan@users.noreply.github.com>
2021-09-30 20:18:59 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(name='dummy download',
+      version='1',
+      description='Setting up setup.py',
+      url='https://github.com/angelaho0504/CSC510_HW2b',
+      author='Hrushabhsingh Chouhan, Hiral Bhanushali, Apoorva Chauhan, Angela Ho',
+      author_email='hychouha@ncsu.edu',
+      license='MIT',
+      packages=find_packages(exclude=['js', 'node_modules', 'tests']),
+      python_requires='>=3.5',
+      install_requires=[
+          'flask',
+          'lime',
+          'argparse',
+          'pathlib',
+          'typing',
+          'numpy',
+          'sklearn',
+          'scipy',
+          'spacy',
+          'tqdm',
+          'pandas',
+          'cython',
+          'textblob',
+      ],
+      include_package_data=True,
+      zip_safe=False)
